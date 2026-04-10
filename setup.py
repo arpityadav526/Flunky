0154275b6cfc1fdc11961aee2f16483eaa686a84
Arpit Yadav <arpit526yadav@gmail.com>
2026-04-11 00:48:41 +0530

    cli installed on mac terminal

--- setup.py
+++ setup.py
@@ -1,20 +1,24 @@
-
 from setuptools import setup, find_packages
 
 setup(
     name="flunky",
-    version="1.0.0",
+    version="0.1.0",
     packages=find_packages(),
     install_requires=[
         "fastapi",
         "uvicorn",
         "sqlalchemy",
         "pydantic",
-        "python-jose[cryptography]",
+        "python-jose",
         "passlib[bcrypt]",
+        "python-dotenv",
+        "python-multipart",
         "typer",
         "rich",
         "httpx",
+        "requests",
+        "email-validator",
+        "jinja2",
     ],
     entry_points={
         "console_scripts": [
